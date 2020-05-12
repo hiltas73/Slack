79533878750e0ce59d8e67082e0e2080e73c98e9
hiltas73 <h_iltas@yahoo.com>
2020-05-12 18:57:55 +0300

    update 2nd

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -3,5 +3,6 @@ package day_1;
 public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("1st created project and class now");
+        System.out.println("2nd changes");
     }
 }
