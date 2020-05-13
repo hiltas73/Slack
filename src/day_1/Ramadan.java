e9350e0b9be4b3e571e0b98fc188f7c8d3c08621
hiltas73 <h_iltas@yahoo.com>
2020-05-13 19:41:39 +0300

    add Ramadan.java

---
+++ src/day_1/Ramadan.java
@@ -0,0 +1,9 @@
+package day_1;
+
+public class Ramadan {
+
+    public static void main(String[] args) {
+        String Ramadan = "Ramadan";
+        System.out.println("Ramadan = " + Ramadan);
+    }
+}
