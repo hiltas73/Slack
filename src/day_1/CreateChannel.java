e805400dda2a6a798979766d2b4d61878a3e4586
halim iltas <h_iltas@yahoo.com>
2020-05-13 17:57:59 +0300

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,5 +4,14 @@ public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("1st created project and class now");
         System.out.println("2nd changes");
+        
+        String github="GitHub";
+        
+        if (github.contains("i")){
+            System.out.println("GitHub = " + github);
+            System.out.println("true = " + true);
+        }else {
+            System.out.println("false = " + false);
+        }
     }
 }
