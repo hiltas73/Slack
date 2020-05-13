3647f1967f158e6fc2a2d3d5a9e61e4aa072b11d
hiltas73 <h_iltas@yahoo.com>
2020-05-13 18:06:35 +0300

    update

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,5 +4,14 @@ public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("1st created project and class now");
         System.out.println("2nd changes");
+
+        String str="str";
+
+        if (str.contains("s")){
+            System.out.println("str = " + str);
+            System.out.println("true = " + true);
+        }else {
+            System.out.println("false = " + false);
+        }
     }
 }
